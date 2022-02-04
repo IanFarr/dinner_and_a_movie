ebfec5dc04637e10e186144fb483f52a8557f092
ian <ianfarr44@gmail.com>
2022-02-03 17:25:06 -0800

    location testing

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = 3000
 app.use(express.json());
 
 app.post('/api/movie', (req, res) => {
-  // console.log(req.body)
   var options = {
     method: 'GET',
     url: 'https://streaming-availability.p.rapidapi.com/search/basic',
@@ -28,22 +27,10 @@ app.post('/api/movie', (req, res) => {
   axios.request(options).then(function (response) {
     console.log(response.data)
     res.json(response.data)
-    // const result = response.data.results[0];
-    // printInfo(result);
   }).catch(function (error) {
     console.error(error);
   });
-
-  function printInfo(result) {
-    const title = result.title;
-    const description = result.overview;
-    const poster = result.posterURLs.original;
-
-    const info = `Title: ${[title]}, Description: ${[description]}, Poster: ${poster}`;
-    console.log(info);
-
-  }
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
