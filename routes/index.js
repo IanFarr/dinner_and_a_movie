e165609411080f2470a11e9862041eb6ac43a4e9
ian <ianfarr44@gmail.com>
2022-02-05 10:15:35 -0800

    movie selection criteria post route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var axios = require("axios").default;
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+// POST movie criteria selections
+router.post('/', function (req, res, next) {
+  const streamServices = req.body.stream_service;
+  const genres = req.body.genre;
+
+  res.send({
+    'streamServices': streamServices,
+    'genres': genres,
+  });
 });
 
 
